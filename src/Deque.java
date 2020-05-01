f7b5ff64b2cbc2cc2c8255191715892b7995d3e6
John Louis Rumingan <jmrumingan@gmail.com>
2020-05-01 13:57:00 -0400

    Create Deque class

---
+++ src/Deque.java
@@ -0,0 +1,10 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.prince.cs.algs4.StdOut;
+
+
+public class Deque<Item> implements Iterable<Item> {
+
+}
